72a502c6a41840ecc9b7147e2127d1f788810dd9
Nocab0819 <penny013101310131@gmail.com>
2023-05-23 14:27:10 +0800

    使用ChatGPT修正按鈕功能
    指令：
    （附上原程式碼）
    目前再玩一次和結束的按鈕沒有功能，要如何讓他有功能

--- word_game.py
+++ word_game.py
@@ -13,6 +13,7 @@ pygame.display.set_caption("拼字遊戲")
 # 定義顏色
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
 
 # 定義字型和字型大小
 FONT_SIZE = 40
@@ -37,7 +38,7 @@ for _ in range(rounds):
     word, scrambled_word = generate_word()
     input_text = ""
     confirmed = False
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,14 +54,14 @@ for _ in range(rounds):
                     input_text = input_text[:-1]
                 else:
                     input_text += event.unicode
-        
+
         window.fill(WHITE)
         input_surface = font.render(input_text, True, BLACK)
         window.blit(input_surface, (20, 20))
         word_surface = font.render(scrambled_word, True, BLACK)
         window.blit(word_surface, (20, 100))
         pygame.display.update()
-        
+
         if confirmed and event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
             input_text = ""
             confirmed = False
@@ -71,17 +72,67 @@ final_score_surface = font.render("Final Score: " + str(score), True, BLACK)
 window.blit(final_score_surface, (20, 200))
 pygame.display.update()
 
-# 等待使用者選擇再玩一次或結束
+# 創建 "再玩一次" 和 "結束" 按鈕的矩形
+replay_button_rect = pygame.Rect(20, 300, 200, 50)
+exit_button_rect = pygame.Rect(20, 350, 200, 50)
+
 while True:
+    replay_button_color = GRAY if replay_button_rect.collidepoint(pygame.mouse.get_pos()) else WHITE
+    exit_button_color = GRAY if exit_button_rect.collidepoint(pygame.mouse.get_pos()) else WHITE
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    
-    # 這裡可以顯示再玩一次或結束的選項，例如按鈕或文字
-    # 這裡只是一個示例，你可以根據需要進行修改
-    replay_surface = font.render("Play Again", True, BLACK)
-    exit_surface = font.render("Exit", True, BLACK)
-    window.blit(replay_surface, (20, 300))
-    window.blit(exit_surface, (20, 350))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if replay_button_rect.collidepoint(mouse_pos):
+                score = 0  # 重置分數
+                for _ in range(rounds):  # 再玩一次
+                    word, scrambled_word = generate_word()
+                    input_text = ""
+                    confirmed = False
+                    while True:
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                pygame.quit()
+                                quit()
+                            elif event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_RETURN:
+                                    if not confirmed:
+                                        if input_text.strip().lower() == word:
+                                            score += 1
+                                        confirmed = True
+                                elif event.key == pygame.K_BACKSPACE:
+                                    input_text = input_text[:-1]
+                                else:
+                                    input_text += event.unicode
+
+                        window.fill(WHITE)
+                        input_surface = font.render(input_text, True, BLACK)
+                        window.blit(input_surface, (20, 20))
+                        word_surface = font.render(scrambled_word, True, BLACK)
+                        window.blit(word_surface, (20, 100))
+                        pygame.display.update()
+
+                        if confirmed and event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                            input_text = ""
+                            confirmed = False
+                            break
+            elif exit_button_rect.collidepoint(mouse_pos):
+                pygame.quit()
+                quit()
+
+    # 重新設定結束畫面
+    window.fill(WHITE)
+    replay_button_surface = font.render("Play Again", True, BLACK)
+    exit_button_surface = font.render("Exit", True, BLACK)
+    window.blit(replay_button_surface, (replay_button_rect.x + 10, replay_button_rect.y + 10))
+    window.blit(exit_button_surface, (exit_button_rect.x + 10, exit_button_rect.y + 10))
+    score_surface = font.render("Score: " + str(score), True, BLACK)
+    window.blit(score_surface, (20, 250))
+    pygame.draw.rect(window, replay_button_color, replay_button_rect)
+    pygame.draw.rect(window, exit_button_color, exit_button_rect)
+    window.blit(replay_button_surface, (replay_button_rect.x + 10, replay_button_rect.y + 10))
+    window.blit(exit_button_surface, (exit_button_rect.x + 10, exit_button_rect.y + 10))
     pygame.display.update()
