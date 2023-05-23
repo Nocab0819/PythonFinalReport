e382fe60b886c13225370f5cf30861c79344949b
Nocab0819 <penny013101310131@gmail.com>
2023-05-23 14:26:09 +0800

    修正錯誤並添加功能
    修正尚未按下enter就換下議題的錯誤
    修正回答錯誤時按下enter無法換下一題的錯誤
    新增結束後顯示畫面

--- word_game.py
+++ word_game.py
@@ -4,92 +4,39 @@ import random
 # 初始化 Pygame
 pygame.init()
 
-# 設定遊戲視窗大小
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+# 設置視窗大小和標題
+WIDTH = 800
+HEIGHT = 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("拼字遊戲")
 
 # 定義顏色
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# 設定字體
-FONT_SIZE = 48
+# 定義字型和字型大小
+FONT_SIZE = 40
 font = pygame.font.Font(None, FONT_SIZE)
 
-# 定義遊戲參數
-rounds = 3  # 遊戲回合數
-score = 0  # 分數
-current_round = 1  # 目前回合數
+# 單字庫
+word_list = ["apple", "banana", "cherry", "durian", "elderberry", "fig", "grape"]
 
+# 生成單字和打亂字母順序
 def generate_word():
-    # 從單字清單中隨機選擇一個單字
-    word_list = ["apple", "banana", "cherry", "date", "elderberry"]
     word = random.choice(word_list)
-    
-    # 打亂單字的字母順序
     word_letters = list(word)
     random.shuffle(word_letters)
     scrambled_word = "".join(word_letters)
-    
     return word, scrambled_word
 
-def display_message(text):
-    # 在視窗中心顯示一則訊息
-    text_surface = font.render(text, True, BLACK)
-    text_rect = text_surface.get_rect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-    window.blit(text_surface, text_rect)
-    pygame.display.update()
+# 遊戲迴圈
+score = 0
+rounds = 3
 
-def game_loop():
-    global score, current_round
-    
-    while current_round <= rounds:
-        # 遊戲迴圈
-        
-        # 重設遊戲狀態
-        word, scrambled_word = generate_word()
-        input_text = ""
-        
-        while True:
-            # 處理事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        # 確認輸入是否正確
-                        if input_text.lower() == word:
-                            score += 1
-                        break
-                    elif event.key == pygame.K_BACKSPACE:
-                        # 刪除最後一個輸入的字母
-                        input_text = input_text[:-1]
-                    else:
-                        # 添加輸入的字母到輸入文字串
-                        input_text += event.unicode
-            
-            # 更新遊戲畫面
-            window.fill(WHITE)
-            input_surface = font.render(input_text, True, BLACK)
-            window.blit(input_surface, (20, 20))
-            word_surface = font.render(scrambled_word, True, BLACK)
-            window.blit(word_surface, (20, 100))
-            pygame.display.update()
-            
-            if input_text.lower() == word:
-                break
-        
-        # 進入下一回合
-        current_round += 1
-    
-    # 顯示最終分數
-    display_message("遊戲結束，你的分數為: {}".format(score))
-    
-    # 顯示再玩一次或結束的選項
-    display_message("再玩一次（R）或結束（Q）？")
+for _ in range(rounds):
+    word, scrambled_word = generate_word()
+    input_text = ""
+    confirmed = False
     
     while True:
         for event in pygame.event.get():
@@ -97,15 +44,44 @@ def game_loop():
                 pygame.quit()
                 quit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    # 重新開始遊戲
-                    score = 0
-                    current_round = 1
-                    game_loop()
-                elif event.key == pygame.K_q:
-                    # 結束遊戲
-                    pygame.quit()
-                    quit()
+                if event.key == pygame.K_RETURN:
+                    if not confirmed:
+                        if input_text.strip().lower() == word:
+                            score += 1
+                        confirmed = True
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+                else:
+                    input_text += event.unicode
+        
+        window.fill(WHITE)
+        input_surface = font.render(input_text, True, BLACK)
+        window.blit(input_surface, (20, 20))
+        word_surface = font.render(scrambled_word, True, BLACK)
+        window.blit(word_surface, (20, 100))
+        pygame.display.update()
+        
+        if confirmed and event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            input_text = ""
+            confirmed = False
+            break
 
-# 開始遊戲
-game_loop()
+# 顯示最終分數
+final_score_surface = font.render("Final Score: " + str(score), True, BLACK)
+window.blit(final_score_surface, (20, 200))
+pygame.display.update()
+
+# 等待使用者選擇再玩一次或結束
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    
+    # 這裡可以顯示再玩一次或結束的選項，例如按鈕或文字
+    # 這裡只是一個示例，你可以根據需要進行修改
+    replay_surface = font.render("Play Again", True, BLACK)
+    exit_surface = font.render("Exit", True, BLACK)
+    window.blit(replay_surface, (20, 300))
+    window.blit(exit_surface, (20, 350))
+    pygame.display.update()
