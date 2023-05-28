e92338c07b33e9a169544a744b88191e17fbe3cc
蔡沛恩 <nocab@caipeiendeMacBook-Pro.local>
2023-05-28 23:53:15 +0800

    建立好基本遊戲頁面（首頁、遊戲頁面、分數頁面）
    目前首頁至遊戲頁面以及分數頁面至遊戲頁面的按鈕功能尚有問題未解決（會出現Bad Request）

--- app.py
+++ app.py
@@ -1,49 +1,63 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+import os
 import random
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-words = ["apple", "banana", "cherry"]  # 單字庫
-rounds = 3  # 回合數
+# 可自行設定回合數和文字內容
+rounds = 3
+words = ['apple', 'banana', 'cherry', 'orange', 'mango']
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # 如果收到 POST 請求，將使用者導向遊戲頁面
+    if request.method == 'POST':
+        return redirect('/game')
+    else:
+        session.clear()
+        return render_template('welcome.html')
 
-@app.route('/play', methods=['GET', 'POST'])
-def play_game():
-    score = 0  # 分數
-    current_round = 1  # 目前回合
-    current_word = ""  # 目前的單字
-    scrambled_word = ""  # 打亂順序後的單字
-
-    def get_random_word():
-        return random.choice(words)
-
-    def scramble_word(word):
-        letters = list(word)
-        random.shuffle(letters)
-        return ''.join(letters)
-
-    def check_answer(user_answer):
-        return user_answer.lower() == current_word.lower()
-
-    def update_game_state():
-        nonlocal score, current_round, current_word, scrambled_word
-        if current_round <= rounds:
-            current_word = get_random_word()
-            scrambled_word = scramble_word(current_word)
-        else:
-            current_word = ""
-            scrambled_word = ""
-        score += 1
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    # 檢查是否有計分的會話，若無則初始化相關會話變數
+    if 'score' not in session:
+        session['score'] = 0
+        session['current_round'] = 1
+        session['current_word'] = random.choice(words)
+        session['shuffled_word'] = list(session['current_word'])
+        random.shuffle(session['shuffled_word'])
+        session['shuffled_word'] = ''.join(session['shuffled_word'])
 
     if request.method == 'POST':
-        user_answer = request.form['answer']
-        if check_answer(user_answer):
-            update_game_state()
+        # 取得使用者輸入的單字並轉換為小寫
+        user_word = request.form['word'].lower()
+        if user_word == session['current_word']:
+            session['score'] += 1
 
-    return render_template('game.html', score=score, word=current_word, scrambled_word=scrambled_word)
+        if session['current_round'] < rounds:
+            # 若還有回合，則更新相關會話變數
+            session['current_round'] += 1
+            session['current_word'] = random.choice(words)
+            session['shuffled_word'] = list(session['current_word'])
+            random.shuffle(session['shuffled_word'])
+            session['shuffled_word'] = ''.join(session['shuffled_word'])
+        else:
+            # 若回合結束，導向分數結果頁面
+            return redirect('/score/result')
+
+    return render_template('game.html', shuffled_word=session['shuffled_word'], round=session['current_round'])
+
+@app.route('/score/result', methods=['GET', 'POST'])
+def show_score():
+    if request.method == 'POST':
+        # 若收到 POST 請求，則清除會話並重新導向遊戲頁面
+        session.clear()
+        return redirect('/game')
+
+    score = session['score']
+    return render_template('score.html', score=score, rounds=rounds)
+
 
 if __name__ == '__main__':
     app.run()
