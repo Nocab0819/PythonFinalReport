eaa56787a1fde012268a85a13bf0880f11d0766b
蔡沛恩 <penny013101310131@gmail.com>
2023-06-01 14:56:22 +0800

    增加功能說明及註解

--- setmode/config.py
+++ setmode/config.py
@@ -1,9 +1,13 @@
+"""
+檔名：config.py
+功能：提供拼字遊戲所使用的單字列表，方便修改內容。
+"""
 def get_words():
     # 可自行填入字詞
     word = [
         'apple', 'banana', 'cherry', 'orange', 'mango',
-        'strawberry', 'kiwi', 'pineapple', 'grape',
-        'blueberry', 'pear', 'peach', 'avocado',
-        'coconut', 'papaya', 'guava',  'watermelon'
+        'strawberry', 'kiwi', 'pineapple', 'grape', 'plum', 
+        'blueberry', 'pear', 'peach', 'avocado', 'pomelo',
+        'coconut', 'papaya', 'guava',  'watermelon', 'tomato'
     ]
     return word
